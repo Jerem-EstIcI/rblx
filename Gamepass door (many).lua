8cb6bc32632097b4ae1a85bcb0c4ce2695b774bd
jeremy <114385056+Jerem-EstIcI@users.noreply.github.com>
2023-03-15 07:55:31 +0100

    Add files via upload

---
+++ Gamepass door (many).lua
@@ -0,0 +1,38 @@
+local MS = game:GetService("MarketplaceService")
+local Gamepasses = {
+	{ id = 18857464, name = "VIP" },
+	{ id = 18857399, name = "MiniVIP" }
+}
+local OpenTime = 1 -- The time the door is open for.
+local OpenTrans = 0.5 -- The transparency of the door when it is open.
+local CloseTrans = 0 -- The transparency of the door when it is closed.
+local BuyGUI = true -- Set to false to stop the BuyGUI appearing.
+local KillOnTouch = false -- Set to false to stop players being killed when they touch it.
+
+local Door = script.Parent -- The door
+
+Door.Touched:Connect(function(hit)
+	if game.Players:GetPlayerFromCharacter(hit.Parent) then
+		local plr = game.Players:GetPlayerFromCharacter(hit.Parent)
+		local owns_gamepass = false
+		for _, gamepass in ipairs(Gamepasses) do
+			if MS:UserOwnsGamePassAsync(plr.UserId, gamepass.id) then
+				owns_gamepass = true
+				break
+			end
+		end
+		if owns_gamepass then
+			Door.CanCollide, Door.Transparency = false, OpenTrans
+			wait(OpenTime)
+			Door.CanCollide, Door.Transparency = true, CloseTrans
+		else
+			Door.CanCollide, Door.Transparency = true, CloseTrans
+			if BuyGUI == true then
+				MS:PromptGamePassPurchase(plr, Gamepasses[1].id)
+			end
+			if KillOnTouch == true then
+				plr.Character.Humanoid.Health = 0
+			end
+		end
+	end
+end)
---
+++ Gamepass tchat tag (many).lua
@@ -0,0 +1,40 @@
+local gamepassIdVIP = 18857464 -- Gamepass VIP ID
+local gamepassIdMiniVIP = 18857399 -- Gamepass Mini VIP ID
+
+local service = game:GetService("MarketplaceService")
+
+game.Players.PlayerAdded:Connect(function(player)
+	if service:UserOwnsGamePassAsync(player.UserId, gamepassIdVIP) then
+		local tags = {
+			{
+				TagText = "VIP", -- Tag
+				TagColor = Color3.fromRGB(255, 255, 0) -- VIP Color
+			}
+		}
+		local ChatService = require(game:GetService("ServerScriptService"):WaitForChild("ChatServiceRunner").ChatService)
+		local speaker = nil
+		while speaker == nil do
+			speaker = ChatService:GetSpeaker(player.Name)
+			if speaker ~= nil then break end
+			wait(0.01)
+		end
+		speaker:SetExtraData("Tags", tags)
+		speaker:SetExtraData("ChatColor", Color3.fromRGB(255, 255, 0)) -- Text Color
+	elseif service:UserOwnsGamePassAsync(player.UserId, gamepassIdMiniVIP) then
+		local tags = {
+			{
+				TagText = "Mini VIP", -- Tag
+				TagColor = Color3.fromRGB(0, 255, 0) -- Mini VIP Color
+			}
+		}
+		local ChatService = require(game:GetService("ServerScriptService"):WaitForChild("ChatServiceRunner").ChatService)
+		local speaker = nil
+		while speaker == nil do
+			speaker = ChatService:GetSpeaker(player.Name)
+			if speaker ~= nil then break end
+			wait(0.01)
+		end
+		speaker:SetExtraData("Tags", tags)
+		speaker:SetExtraData("ChatColor", Color3.fromRGB(0, 255, 0)) -- Text Color
+	end
+end)
